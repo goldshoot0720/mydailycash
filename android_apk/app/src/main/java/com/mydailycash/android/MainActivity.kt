41da026060a407de39f65e35bd9f0ec76f5bc046
goldshoot0720 <goldshoot0720@users.noreply.github.com>
2026-04-03 01:07:12 +0800

    Fix Android jackpot banner sizing for CI build

--- android_apk/app/src/main/java/com/mydailycash/android/MainActivity.kt
+++ android_apk/app/src/main/java/com/mydailycash/android/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.BoxScope.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -860,7 +859,7 @@ private fun JackpotBanner(glowAlpha: Float) {
     ) {
         JackpotSparkles(
             modifier = Modifier
-                .matchParentSize()
+                .fillMaxSize()
                 .alpha(0.9f),
             glowAlpha = glowAlpha,
         )
